340c207d16c57b48face6bba4c0605f3381563dc
mahme <mustafasaleh.hf@gmail.com>
2021-01-25 17:25:34 +0200

    dockerize the api

--- controllers/index.js
+++ controllers/index.js
@@ -10,7 +10,7 @@ function homePage(req, res, next) {
   const response = {
     data: {
       message: "Welcome to web_api_base api",
-      version: "0.1.0",
+      version: "1.0.1",
     },
   };
   successHandler(res, response);
